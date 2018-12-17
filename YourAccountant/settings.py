c4558751d8fd4e7d85985dbfc0146a2bec531a46
Volodymyr Vasylyk <Vasylyk_volodya@ukr.net>
2018-12-17 17:26:54 +0200

    add models: Specialization, Advantages, HowWeWork

--- YourAccountant/settings.py
+++ YourAccountant/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -27,78 +26,75 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'accountant.apps.AccountantConfig',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'YourAccountant.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+			],
+		},
+	},
 ]
 
 WSGI_APPLICATION = 'YourAccountant.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	}
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+	{
+		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+	},
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
--- accountant/models.py
+++ accountant/models.py
@@ -34,5 +34,55 @@ class AboutUs(models.Model):
 		verbose_name = 'Про нас'
 		verbose_name_plural = 'Про нас'
 
+	title = models.CharField(max_length=255, blank=False, null=False)
 	text = models.TextField(blank=False, null=False)
+	description = models.TextField(blank=False, null=False)
+	created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+	updated = models.DateTimeField(auto_now=True, blank=True)
 
+	def __str__(self):
+		return self.title
+
+
+class Specialization(models.Model):
+	class Meta:
+		verbose_name = 'Наша спеціалізація'
+		verbose_name_plural = 'Наша спеціалізація'
+
+	img = models.ImageField(max_length=255, default='', blank=True, null=True)
+	title = models.CharField(max_length=255, blank=False, null=False)
+	created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+	updated = models.DateTimeField(auto_now=True, blank=True)
+
+	def __str__(self):
+		return self.title
+
+
+class Advantages(models.Model):
+	class Meta:
+		verbose_name = 'Перевага співпраці'
+		verbose_name_plural = 'Переваги співпраці'
+
+	img = models.ImageField(max_length=255, default='', blank=True, null=True)
+	title = models.CharField(max_length=255, blank=False, null=False)
+	text = models.TextField(blank=False, null=False)
+	created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+	updated = models.DateTimeField(auto_now=True, blank=True)
+
+	def __str__(self):
+		return self.title
+
+
+class HowWeWork(models.Model):
+	class Meta:
+		verbose_name = 'Як ми працюємо'
+		verbose_name_plural = 'Як ми працюємо'
+
+	img = models.ImageField(max_length=255, default='', blank=True, null=True)
+	title = models.CharField(max_length=255, blank=False, null=False)
+	text = models.TextField(blank=False, null=False)
+	created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+	updated = models.DateTimeField(auto_now=True, blank=True)
+
+	def __str__(self):
+		return self.title
