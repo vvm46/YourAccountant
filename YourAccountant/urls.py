6fb2229eb6c1d74926b655f918c3d02a04730ea1
Volodymyr Vasylyk <Vasylyk_volodya@ukr.net>
2018-12-19 16:49:53 +0200

    add change to views.py

--- YourAccountant/urls.py
+++ YourAccountant/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accountant.views import MainView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', MainView.as_view(), name='index')
 ]
--- accountant/views.py
+++ accountant/views.py
@@ -1,111 +1,70 @@
-
 from django.http import JsonResponse
 from django.core.mail import EmailMessage
-from django.views.generic import TemplateView, ListView, FormView, DetailView
+from django.views.generic import TemplateView, ListView, FormView
 from accountant.models import Title, Services, AboutUs, Specialization, Advantages, HowWeWork, ComplexServices, \
-	Contacts, Applications
+    Contacts, Applications, Activities, OrganizationalForm, TaxSystem
 from accountant.forms import ApplicationsForm
 
 
-class TitleView(TemplateView):
-	template_name = ''
+class MainView(FormView):
+    template_name = ''
 
-	def get_context_data(self, **kwargs):
-		context = super(TitleView, self).get_context_data(**kwargs)
-		context['title'] = Title.objects.last()
-		return context
+    def get_context_data(self, **kwargs):
+        context = super(MainView, self).get_context_data(**kwargs)
+        context['title'] = Title.objects.last()
+        context['services'] = Services.objects.filter(enabled=True)
+        context['about_us'] = AboutUs.objects.last()
+        context['specializations'] = Specialization.objects.filter(enabled=True)
+        context['advantages'] = Advantages.objects.filter(enabled=True)
+        context['how_we_works'] = HowWeWork.objects.filter(enabled=True)
+        context['complex_service'] = ComplexServices.objects.last()
+        context['contact'] = Contacts.objects.last()
+        context['activities'] = Activities.objects.all()
+        context['organizational_form'] = OrganizationalForm.objects.all()
+        context['tax_systems'] = TaxSystem.objects.all()
+        return context
 
-
-class ServicesView(ListView):
-	template_name = ''
-	model = Services
-
-	def get_context_data(self, **kwargs):
-		context = super(ServicesView, self).get_context_data(**kwargs)
-		context['services'] = Services.objects.filter(enabled=True)
-		return context
-
-
-class AboutUsView(TemplateView):
-	template_name = ''
-
-	def get_context_data(self, **kwargs):
-		context = super(AboutUsView, self).get_context_data(**kwargs)
-		context['about_us'] = AboutUs.objects.last()
-		return context
-
-
-class SpecializationView(ListView):
-	template_name = ''
-	model = Specialization
-
-	def get_context_data(self, **kwargs):
-		context = super(SpecializationView, self).get_context_data(**kwargs)
-		context['specializations'] = Specialization.objects.filter(enabled=True)
-		return context
-
-
-class AdvantagesView(ListView):
-	template_name = ''
-	model = Advantages
-
-	def get_context_data(self, **kwargs):
-		context = super(AdvantagesView, self).get_context_data(**kwargs)
-		context['advantages'] = Advantages.objects.filter(enabled=True)
-		return context
+    def post(self, request, *args, **kwargs):
+        if request.POST:
+            form = ApplicationsForm(request.POST)
+            if form.is_valid():
+                form.save()
+                name = form.cleaned_data['name']
+                phone = form.cleaned_data['phone']
+                description = form.cleaned_data['description']
+                text = 'Імя:   {0}\nТелефон:   {1}\nПовідомлення:   {2}'.format(name, phone, description)
+                try:
+                    mail = EmailMessage(
+                        name, text, to=['XXX@gmail.com']  # TODO: MUST CHANGE FOR REAL MAIL!!!
+                    )
+                    mail.send()
+                except Exception as e:
+                    print(e)
+                return JsonResponse({'status': True})
+            else:
+                return JsonResponse({'status': False, 'message': form.errors['captcha'][0]})
+        return super(MainView, self).get(request, *args, **kwargs)
 
 
-class HowWeWorkView(ListView):
-	template_name = ''
-	model = HowWeWork
-
-	def get_context_data(self, **kwargs):
-		context = super(HowWeWorkView, self).get_context_data(**kwargs)
-		context['how_we_works'] = HowWeWork.objects.filter(enabled=True)
-		return context
-
-
-class ComplexServicesView(TemplateView):
-	template_name = ''
-	model = ComplexServices
-
-	def get_context_data(self, **kwargs):
-		context = super(ComplexServicesView, self).get_context_data(**kwargs)
-		context['complex_service'] = ComplexServices.objects.last()
-		return context
-
-
-class ContactsView(TemplateView):
-	template_name = ''
-	model = Contacts
-
-	def get_context_data(self, **kwargs):
-		context = super(ContactsView, self).get_context_data(**kwargs)
-		context['complex_service'] = Contacts.objects.last()
-		return context
-
-
-class ApplicationsView(FormView):
-	template_name = ''
-	form_class = ApplicationsForm
-
-	def post(self, request, *args, **kwargs):
-		if request.POST:
-			form = ApplicationsForm(request.POST)
-			if form.is_valid():
-				form.save()
-				name = form.cleaned_data['name']
-				phone = form.cleaned_data['phone']
-				description = form.cleaned_data['description']
-				text = 'Імя:   {0}\nТелефон:   {1}\nПовідомлення:   {2}'.format(name, phone, description)
-				try:
-					mail = EmailMessage(
-						name, text, to=['XXX@gmail.com']  # TODO: MUST CHANGE FOR REAL MAIL!!!
-					)
-					mail.send()
-				except Exception as e:
-					print(e)
-				return JsonResponse({'status': True})
-			else:
-				return JsonResponse({'status': False, 'message': form.errors['captcha'][0]})
-		return super(ApplicationsView, self).get(request, *args, **kwargs)
+# class ActivitiesView(ListView):
+#     model = Activities
+#
+#     def get_queryset(self):
+#         objects = Activities.objects.all()
+#         return objects
+#
+#
+# class OrganizationalFormView(ListView):
+#     model = OrganizationalForm
+#
+#     def get_queryset(self):
+#         objects = OrganizationalForm.objects.all()
+#         return objects
+#
+#
+# class TaxSystemView(ListView):
+#     model = TaxSystem
+#
+#     def get_queryset(self):
+#         objects = TaxSystem.objects.all()
+#         return objects
