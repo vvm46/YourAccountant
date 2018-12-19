5807457581c53b53b607ad3ba95313583c413343
Volodymyr Vasylyk <Vasylyk_volodya@ukr.net>
2018-12-19 15:31:23 +0200

    register models in admin

--- accountant/admin.py
+++ accountant/admin.py
@@ -2,75 +2,111 @@ from django.contrib import admin
 from modeltranslation.admin import TabbedTranslationAdmin
 from django.template.defaultfilters import mark_safe
 from accountant.models import Title, Services, AboutUs, Specialization, Advantages, HowWeWork, ComplexServices, \
-	Contacts, Applications
+    Contacts, Applications
 
 
 class TitleAdmin(TabbedTranslationAdmin):
-	list_display = ['id', 'title_uk', 'created', 'updated', 'enabled']
-	list_display_links = ['id', 'title_uk']
-	list_editable = ['enabled']
-	readonly_fields = ['created', 'updated']
+    list_display = ['id', 'title_uk', 'created', 'updated', 'enabled']
+    list_display_links = ['id', 'title_uk']
+    list_editable = ['enabled']
+    readonly_fields = ['created', 'updated']
 
 
 class ServicesAdmin(TabbedTranslationAdmin):
-	list_display = ['id', 'title_uk', 'text_uk', 'created', 'updated', 'enabled']
-	list_display_links = ['id', 'title_uk', 'text_uk']
-	list_editable = ['enabled']
-	readonly_fields = ['picture_priview', 'created', 'updated']
+    list_display = ['id', 'title_uk', 'text_uk', 'created', 'updated', 'enabled']
+    list_display_links = ['id', 'title_uk', 'text_uk']
+    list_editable = ['enabled']
+    readonly_fields = ['picture_priview', 'created', 'updated']
 
-	def picture_priview(self, obj):
-		try:
-			return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
-				url=obj.img.url,
-				width=obj.img.width,
-				height=obj.img.height,
-			)
-			)
-		except FileNotFoundError as e:
-			return e
+    def picture_priview(self, obj):
+        try:
+            return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+                url=obj.img.url,
+                width=obj.img.width,
+                height=obj.img.height,
+            )
+            )
+        except FileNotFoundError as e:
+            return e
 
 
 class AboutUsAdmin(TabbedTranslationAdmin):
-	list_display = ['id', 'title_uk', 'text_uk', 'description_uk', 'created', 'updated', 'enabled']
-	list_display_links = ['id', 'title_uk', 'text_uk', 'description_uk']
-	list_editable = ['enabled']
-	readonly_fields = ['created', 'updated']
+    list_display = ['id', 'title_uk', 'text_uk', 'description_uk', 'created', 'updated', 'enabled']
+    list_display_links = ['id', 'title_uk', 'text_uk', 'description_uk']
+    list_editable = ['enabled']
+    readonly_fields = ['created', 'updated']
 
 
 class SpecializationAdmin(TabbedTranslationAdmin):
-	list_display = ['id', 'title_uk', 'created', 'updated', 'enabled']
-	list_display_links = ['id', 'title_uk']
-	list_editable = ['enabled']
-	readonly_fields = ['picture_priview', 'created', 'updated']
+    list_display = ['id', 'title_uk', 'created', 'updated', 'enabled']
+    list_display_links = ['id', 'title_uk']
+    list_editable = ['enabled']
+    readonly_fields = ['picture_priview', 'created', 'updated']
 
-	def picture_priview(self, obj):
-		try:
-			return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
-				url=obj.img.url,
-				width=obj.img.width,
-				height=obj.img.height,
-			)
-			)
-		except FileNotFoundError as e:
-			return e
+    def picture_priview(self, obj):
+        try:
+            return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+                url=obj.img.url,
+                width=obj.img.width,
+                height=obj.img.height,
+            )
+            )
+        except FileNotFoundError as e:
+            return e
 
 
 class AdvantagesAdmin(TabbedTranslationAdmin):
-	list_display = ['id', 'title_uk', 'text_uk', 'created', 'updated', 'enabled']
-	list_display_links = ['id', 'title_uk', 'text_uk']
-	list_editable = ['enabled']
-	readonly_fields = ['picture_priview', 'created', 'updated']
+    list_display = ['id', 'title_uk', 'text_uk', 'created', 'updated', 'enabled']
+    list_display_links = ['id', 'title_uk', 'text_uk']
+    list_editable = ['enabled']
+    readonly_fields = ['picture_priview', 'created', 'updated']
 
-	def picture_priview(self, obj):
-		try:
-			return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
-				url=obj.img.url,
-				width=obj.img.width,
-				height=obj.img.height,
-			)
-			)
-		except FileNotFoundError as e:
-			return e
+    def picture_priview(self, obj):
+        try:
+            return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+                url=obj.img.url,
+                width=obj.img.width,
+                height=obj.img.height,
+            )
+            )
+        except FileNotFoundError as e:
+            return e
+
+
+class HowWeWorkAdmin(TabbedTranslationAdmin):
+    list_display = ['id', 'title_uk', 'text_uk', 'created', 'updated', 'enabled']
+    list_display_links = ['id', 'title_uk', 'text_uk']
+    list_editable = ['enabled']
+    readonly_fields = ['picture_priview', 'created', 'updated']
+
+    def picture_priview(self, obj):
+        try:
+            return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+                url=obj.img.url,
+                width=obj.img.width,
+                height=obj.img.height,
+            )
+            )
+        except FileNotFoundError as e:
+            return e
+
+
+class ComplexServicesAdmin(TabbedTranslationAdmin):
+    list_display = ['id', 'title_uk', 'subtitle_uk', 'additionally_uk', 'enabled']
+    list_display_links = ['id', 'title_uk', 'subtitle_uk', 'additionally_uk']
+    list_editable = ['enabled']
+
+
+class ContactsAdmin(TabbedTranslationAdmin):
+    list_display = ['id', 'time_of_work_uk', 'street_uk', 'email', 'phones', 'map', 'company_footer_uk', 'enabled']
+    list_display_links = ['id', 'time_of_work_uk', 'street_uk', 'email', 'phones']
+    list_editable = ['enabled']
+
+
+class ApplicationsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'phone', 'email', 'activity', 'organizational_form', 'tax_system', 'created']
+    list_display_links = ['id', 'name', 'phone', 'email', 'activity', 'organizational_form', 'tax_system']
+    readonly_fields = ['created']
 
 
 admin.site.register(Title, TitleAdmin)
@@ -78,3 +114,7 @@ admin.site.register(Services, ServicesAdmin)
 admin.site.register(AboutUs, AboutUsAdmin)
 admin.site.register(Specialization, SpecializationAdmin)
 admin.site.register(Advantages, AdvantagesAdmin)
+admin.site.register(HowWeWork, HowWeWorkAdmin)
+admin.site.register(ComplexServices, ComplexServicesAdmin)
+admin.site.register(Contacts, ContactsAdmin)
+admin.site.register(Applications, ApplicationsAdmin)
--- accountant/models.py
+++ accountant/models.py
@@ -2,138 +2,141 @@ from django.db import models
 
 
 class Title(models.Model):
-	class Meta:
-		verbose_name = 'Головна'
-		verbose_name_plural = 'Головна'
+    class Meta:
+        verbose_name = 'Головна'
+        verbose_name_plural = 'Головна'
 
-	title = models.CharField(max_length=255, blank=False, null=False)
-	created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-	updated = models.DateTimeField(auto_now=True, blank=True)
-	enabled = models.BooleanField(default=False, help_text='Дозволити відображати на сайті')
+    title = models.CharField(max_length=255, blank=False, null=False)
+    created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True, blank=True)
+    enabled = models.BooleanField(default=False, help_text='Дозволити відображати на сайті')
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
 
 class Services(models.Model):
-	class Meta:
-		verbose_name = 'Послуга'
-		verbose_name_plural = 'Послуги'
+    class Meta:
+        verbose_name = 'Послуга'
+        verbose_name_plural = 'Послуги'
 
-	title = models.CharField(max_length=255, blank=False, null=False)
-	img = models.ImageField(max_length=255, default='', blank=True, null=True)
-	text = models.TextField(blank=False, null=False)
-	created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-	updated = models.DateTimeField(auto_now=True, blank=True)
-	enabled = models.BooleanField(default=False, help_text='Дозволити відображати на сайті')
+    title = models.CharField(max_length=255, blank=False, null=False)
+    img = models.ImageField(max_length=255, default='', blank=True, null=True)
+    text = models.TextField(blank=False, null=False)
+    created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True, blank=True)
+    enabled = models.BooleanField(default=False, help_text='Дозволити відображати на сайті')
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
 
 class AboutUs(models.Model):
-	class Meta:
-		verbose_name = 'Про нас'
-		verbose_name_plural = 'Про нас'
+    class Meta:
+        verbose_name = 'Про нас'
+        verbose_name_plural = 'Про нас'
 
-	title = models.CharField(max_length=255, blank=False, null=False)
-	text = models.TextField(blank=False, null=False)
-	description = models.TextField(blank=False, null=False)
-	created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-	updated = models.DateTimeField(auto_now=True, blank=True)
-	enabled = models.BooleanField(default=False, help_text='Дозволити відображати на сайті')
+    title = models.CharField(max_length=255, blank=False, null=False)
+    text = models.TextField(blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+    created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True, blank=True)
+    enabled = models.BooleanField(default=False, help_text='Дозволити відображати на сайті')
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
 
 class Specialization(models.Model):
-	class Meta:
-		verbose_name = 'Наша спеціалізація'
-		verbose_name_plural = 'Наша спеціалізація'
+    class Meta:
+        verbose_name = 'Наша спеціалізація'
+        verbose_name_plural = 'Наша спеціалізація'
 
-	img = models.ImageField(max_length=255, default='', blank=True, null=True)
-	title = models.CharField(max_length=255, blank=False, null=False)
-	created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-	updated = models.DateTimeField(auto_now=True, blank=True)
+    img = models.ImageField(max_length=255, default='', blank=True, null=True)
+    title = models.CharField(max_length=255, blank=False, null=False)
+    created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True, blank=True)
+    enabled = models.BooleanField(default=False, help_text='Дозволити відображати на сайті')
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
 
 class Advantages(models.Model):
-	class Meta:
-		verbose_name = 'Перевага співпраці'
-		verbose_name_plural = 'Переваги співпраці'
+    class Meta:
+        verbose_name = 'Перевага співпраці'
+        verbose_name_plural = 'Переваги співпраці'
 
-	img = models.ImageField(max_length=255, default='', blank=True, null=True)
-	title = models.CharField(max_length=255, blank=False, null=False)
-	text = models.TextField(blank=False, null=False)
-	created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-	updated = models.DateTimeField(auto_now=True, blank=True)
-	enabled = models.BooleanField(default=False, help_text='Дозволити відображати на сайті')
+    img = models.ImageField(max_length=255, default='', blank=True, null=True)
+    title = models.CharField(max_length=255, blank=False, null=False)
+    text = models.TextField(blank=False, null=False)
+    created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True, blank=True)
+    enabled = models.BooleanField(default=False, help_text='Дозволити відображати на сайті')
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
 
 class HowWeWork(models.Model):
-	class Meta:
-		verbose_name = 'Як ми працюємо'
-		verbose_name_plural = 'Як ми працюємо'
+    class Meta:
+        verbose_name = 'Як ми працюємо'
+        verbose_name_plural = 'Як ми працюємо'
 
-	img = models.ImageField(max_length=255, default='', blank=True, null=True)
-	title = models.CharField(max_length=255, blank=False, null=False)
-	text = models.TextField(blank=False, null=False)
-	created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-	updated = models.DateTimeField(auto_now=True, blank=True)
-	enabled = models.BooleanField(default=False, help_text='Дозволити відображати на сайті')
+    img = models.ImageField(max_length=255, default='', blank=True, null=True)
+    title = models.CharField(max_length=255, blank=False, null=False)
+    text = models.TextField(blank=False, null=False)
+    created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True, blank=True)
+    enabled = models.BooleanField(default=False, help_text='Дозволити відображати на сайті')
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
 
 class ComplexServices(models.Model):
-	class Meta:
-		verbose_name = 'Комплексне обслуговування'
-		verbose_name_plural = 'Комплексне обслуговування'
+    class Meta:
+        verbose_name = 'Комплексне обслуговування'
+        verbose_name_plural = 'Комплексне обслуговування'
 
-	title = models.CharField(max_length=555, blank=False, null=False)
-	subtitle = models.TextField(blank=False, null=False, default=None)
-	additionally = models.TextField(blank=False, null=False, default=None)
-	enabled = models.BooleanField(default=False, help_text='Дозволити відображати на сайті')
+    title = models.CharField(max_length=555, blank=False, null=False)
+    subtitle = models.TextField(blank=False, null=False, default=None)
+    additionally = models.TextField(blank=False, null=False, default=None)
+    enabled = models.BooleanField(default=False, help_text='Дозволити відображати на сайті')
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
 
 class Contacts(models.Model):
-	class Meta:
-		verbose_name = 'Контакти'
-		verbose_name_plural = 'Контакти'
+    class Meta:
+        verbose_name = 'Контакти'
+        verbose_name_plural = 'Контакти'
 
-	time_of_work = models.CharField(max_length=255, blank=False, null=False, default=None)
-	street = models.CharField(max_length=255, blank=False, null=False, default=None)
-	email = models.EmailField()
-	phones = models.CharField(max_length=255, blank=False, null=False, default=None)
-	company_footer = models.CharField(max_length=455, blank=False, null=False, default=None)
-	map = models.URLField(blank=False, null=False)
+    time_of_work = models.CharField(max_length=255, blank=False, null=False, default=None)
+    street = models.CharField(max_length=255, blank=False, null=False, default=None)
+    email = models.EmailField()
+    phones = models.CharField(max_length=255, blank=False, null=False, default=None)
+    company_footer = models.CharField(max_length=455, blank=False, null=False, default=None)
+    map = models.URLField(blank=False, null=False)
+    enabled = models.BooleanField(default=False, help_text='Дозволити відображати на сайті')
 
-	def __str__(self):
-		return self.time_of_work
+    def __str__(self):
+        return self.time_of_work
 
 
 class Applications(models.Model):
-	class Meta:
-		verbose_name = 'Заявка'
-		verbose_name_plural = 'Заявки'
+    class Meta:
+        verbose_name = 'Заявка'
+        verbose_name_plural = 'Заявки'
 
-	name = models.CharField(max_length=155, blank=False, null=False)
-	phone = models.CharField(max_length=15, blank=False, null=False)
-	email = models.EmailField(blank=False, null=False)
-	activity = models.CharField(max_length=155, blank=False, null=False)  # TODO: replace to chosen menu
-	organizational_form = models.CharField(max_length=155, blank=False, null=False)  # TODO: replace to chosen menu
-	tax_system = models.CharField(max_length=155, blank=False, null=False)  # TODO: replace to chosen menu
+    name = models.CharField(max_length=155, blank=False, null=False)
+    phone = models.CharField(max_length=15, blank=False, null=False)
+    email = models.EmailField(blank=False, null=False)
+    activity = models.CharField(max_length=155, blank=False, null=False)  # TODO: replace to chosen menu
+    organizational_form = models.CharField(max_length=155, blank=False, null=False)  # TODO: replace to chosen menu
+    tax_system = models.CharField(max_length=155, blank=False, null=False)  # TODO: replace to chosen menu
+    created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
